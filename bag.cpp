259be0fbe98d9154c86e470155ef2205401f7344
Vegar Engen <vegar.engen@gmail.com>
2013-03-18 00:43:58 -0700

    cleaned the code

--- bag.cpp
+++ bag.cpp
@@ -31,74 +31,12 @@ public:
 		return bagSize;
 	}
 
-	bool exists(int vertex) {
-		for (int i = 0; i < size(); i++) {
-			if (vertex == bagArray[i]) {
-				return true;
-			}
-		}
-		return false;
-	}
-
 	int peekMin();
 	int popMin();
 
 	void minHeapify(int pos);
 
 	void mergeBags(VertexBag *bag);
-	int* mergeLists(int *a, int *b, int sizeA, int sizeB){
-		printf("Starting mergeLists\n");
-		int* result = new int[sizeA+sizeB];
-		int l =0 ,r =0, size;
-		
-		size = sizeA + sizeB;
-		for (int i = 0; i < size; ++i){
-		//	printf("Appending %i or %i \n", a[l],b[r]);
-			if(a[l] == b[r] && l < sizeA && r < sizeB){
-				result[i] = a[l++];
-				r++;
-				size--;
-			}else if(a[l] < b[r] && l < sizeA && r < sizeB){
-				result[i] = a[l++];
-			}else if(a[l] > b[r]  && l < sizeA && r < sizeB){
-				result[i] = b[r++];
-			}else if(l >= sizeA){
-				result[i] = b[r++];
-			}else if(r >= sizeB){
-				result[i] = a[l++];
-			}
-		}
-		bagSize = size;
-		return result;
-	}
-
-	//Sannsynligvis bug med size hvis det er duplicates
-	int* merge_sort(int *list, int size){
-		int middle = ceil(size/2);
-		int* left = new int[(int)ceil(size/2)];
-		int* right = new int[(int)floor(size/2)];
-
-		if(size <=1){
-			return list;
-		}
-
-		for (int i = 0; i < size; ++i){
-			if(i> ceil(size/2)){
-				left[i] = list[i];
-			}else{
-				right[i-(int)ceil(size/2)] = list[i];
-			}
-		}
-		left = merge_sort(left,ceil(size/2));
-		right = merge_sort(right,floor(size/2));
-		return mergeLists(left,right,ceil(size/2),floor(size/2));
-
-	}
-
-
-	bool isEmpty(){
-		return size() == 0;
-	}
 
 	void put(int vertex) {
 		push(vertex);
@@ -106,21 +44,13 @@ public:
 
 	void push(int vertex);
 
-	int get(); 
-
 	int getElement(int i);
 
-	void setElement(int i, int value);
-
-	void join(VertexBag otherBag) {
-		while (otherBag.size() != 0) {
-			put(otherBag.get());
-		}
-	}
-
-	VertexBag* split(int numberOfBags);
-
 	void printBag();
+
+	bool isEmpty(){
+		return size() == 0;
+	}
 };
 
 VertexBag::VertexBag() {
@@ -132,10 +62,9 @@ VertexBag::VertexBag() {
 }
 
 void VertexBag::printBag(){
-	string str("Bag: ");
 	cout << "Bag: ";
 	for (int i = 0; i <size() && i < 10; i++) {
-		cout << getElement(i) << ", ";
+		cout << bagArray[i] << ", ";
 	}
 	cout << "\n";
 }
@@ -212,20 +141,8 @@ void VertexBag::minHeapify(int i){
 
 }
 
-int VertexBag::get() {
-	if(bagSize != 0){
-		return bagArray[--bagSize];
-	}else{
-		return -1;
-	}
-}
-
 int VertexBag::getElement(int i) {
 	return bagArray[i];
-}
-
-void VertexBag::setElement(int i, int value) {
-	bagArray[i] = value;
 }
 
 void VertexBag::mergeBags(VertexBag *bag){
@@ -234,21 +151,7 @@ void VertexBag::mergeBags(VertexBag *bag){
 	int *temp;
 	int duplicates = 0;
 	temp = new int[size() + bag->size()];
-	//int* left;
-	//left = (int *)calloc(bagSize-1,sizeof(int));
-	//sleep(1);
-	//for sorterte lister
-/*
-	printf("Getting left half\n");
-	for (int i = 0; i < bagSize; ++i){
-		left[i] = getElement(i);
-	}
 
-	printf("Getting right half\n");
-	for (int i = 0; i < bag->size(); ++i){
-		right[i] = bag->getElement(i);
-	}
-*/
 	for (int i = 0; i < size() + bag->size()-duplicates; ++i){
 		if(bag->peekMin() == peekMin() && peekMin() != -1){
 			temp[i] = popMin();
@@ -262,38 +165,5 @@ void VertexBag::mergeBags(VertexBag *bag){
 		//kopiere over verdier til den nye bagen !!!!!!!
 		free(bagArray);
 		bagArray = temp;	
-	}
-	
-	//bagArray = mergeLists(left,right,bagSize,bag->size());
-
-}
-
-VertexBag* VertexBag::split(int numberOfBags){
-
-	VertexBag *bags;
-	bags = new VertexBag[numberOfBags];
-	int numberInEachBag = (int)ceil((double)size()/numberOfBags);
-/*
-	for (int i = 0; i < numberOfBags; i++) {
-
-		// lag settere for bagArray og bagSize
-		bags[i].bagArray = &bagArray[i*numberInEachBag];
-		bags[i].bagSize = numberInEachBag -1;
-		if(i < size() % numberInEachBag){
-			bags[i].bagSize++;
-		}
-	}
-*/
-
-	for (int i = 0; i < numberOfBags; i++) {
-		for (int j = 0; j < numberInEachBag && i*numberInEachBag+j < size(); j++){
-			bags[i].put(getElement(i*numberInEachBag + j));
-		}
-	}
-	return bags;
-
-
-	printf("split done\n");
-	return bags;
-
+	}	
 }
